6650327e23c9558269d9ceb8fec0943db5de35be
Anna Pronina <kiisya1@yandex.ru>
2020-12-11 12:43:56 +0300

    Добавляет js для мобильного меню и правки по pixel perfect

--- source/js/script.js
+++ source/js/script.js
@@ -2,5 +2,55 @@
 
 (function () {
 
+  /* Меню */
+
+  var createFocusTrap = window.createFocusTrap;
+
+  var body = document.querySelector('body');
+  var header = document.querySelector('.header');
+
+  if (header) {
+    header.classList.remove('header--no-js');
+    header.classList.add('header--closed');
+    var menuButton = header.querySelector('.header__menu-toggle');
+
+    menuButton.addEventListener('click', function (evt) {
+      evt.preventDefault();
+
+      if (header.classList.contains('header--closed')) {
+        header.classList.remove('header--closed');
+        body.dataset.scrollY = getBodyScrollTop(); // сохраним значение скролла
+
+        body.classList.add('body-lock');
+
+        if (existVerticalScroll()) {
+          body.style.top = '-' + body.dataset.scrollY + 'px';
+        }
+      } else {
+        header.classList.add('header--closed');
+        body.classList.remove('body-lock');
+
+        if (existVerticalScroll()) {
+          window.scrollTo(0, +body.dataset.scrollY);
+        }
+      }
+
+    });
+  }
+
+  /* Получение положения на странице при открытии поп-апа */
+
+  var existVerticalScroll = function () {
+    return document.body.offsetHeight > window.innerHeight;
+  };
+
+  var getBodyScrollTop = function () {
+    return (
+      self.pageYOffset ||
+      (document.documentElement && document.documentElement.ScrollTop) ||
+      (document.body && document.body.scrollTop)
+    );
+  };
+
 
 })();
