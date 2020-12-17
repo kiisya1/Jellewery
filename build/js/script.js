3a83b1c941573938c55328397c279a3db9f51283
Anna Pronina <kiisya1@yandex.ru>
2020-12-17 12:17:38 +0300

    Добавляет страницу каталога и правит изображения товаров на г лавной

--- build/js/script.js
+++ build/js/script.js
@@ -18,6 +18,15 @@
     var questionsButtons = questionsList.querySelectorAll('.questions__question');
   }
 
+  var filter = document.querySelector('.filter');
+  if (filter) {
+    var filterContainer = filter.querySelector('.filter__container');
+    var filterFocusTrap = createFocusTrap(filterContainer);
+    var filterButtons = filter.querySelectorAll('.filter__feature button[type="button"]');
+    var filterOpenButton = filter.querySelector('.filter__open-button');
+    var filterCloseButton = filter.querySelector('.filter__close-button');
+  }
+
   /* Меню */
 
   if (header) {
@@ -30,20 +39,10 @@
 
       if (header.classList.contains('header--closed')) {
         header.classList.remove('header--closed');
-        body.dataset.scrollY = getBodyScrollTop(); // сохраним значение скролла
-
-        body.classList.add('body-lock');
-
-        if (existVerticalScroll()) {
-          body.style.top = '-' + body.dataset.scrollY + 'px';
-        }
+        blockPage();
       } else {
         header.classList.add('header--closed');
-        body.classList.remove('body-lock');
-
-        if (existVerticalScroll()) {
-          window.scrollTo(0, +body.dataset.scrollY);
-        }
+        unblockPage();
       }
 
     });
@@ -61,6 +60,19 @@
       (document.documentElement && document.documentElement.ScrollTop) ||
       (document.body && document.body.scrollTop)
     );
+  };
+
+  var blockPage = function () {
+    body.dataset.scrollY = getBodyScrollTop(); // сохраним значение скролла
+
+    body.classList.add('body-lock');
+    body.style.top = '-' + body.dataset.scrollY + 'px';
+  };
+
+  var unblockPage = function () {
+    body.classList.remove('body-lock');
+    body.style.top = '';
+    window.scrollTo(0, +body.dataset.scrollY);
   };
 
   // Инициализируем слайдер блока Продукты
@@ -142,6 +154,104 @@
     if (questionsButtons.length !== 0) {
       for (var j = 0; j < questionsButtons.length; j++) {
         addQuestionsButtonClickHandler(questionsButtons[j]);
+      }
+    }
+  }
+
+  // Фильтр
+
+  if (filter) {
+    filter.classList.remove('filter--no-js');
+    var filterButtonsArray = Array.prototype.slice.call(filterButtons, 0);
+
+    filterOpenButton.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      openFilter();
+    });
+
+  }
+
+  var openFilter = function () {
+    if (filterContainer.classList.contains('filter__container--closed')) {
+      blockPage();
+      filterContainer.classList.remove('filter__container--closed');
+      filterFocusTrap.activate();
+    }
+
+    filterCloseButton.addEventListener('click', onFilterCloseButtonClick);
+    filterContainer.addEventListener('click', onFilterContainerOverflowClick);
+    document.addEventListener('keydown', onFilterKeydown);
+  };
+
+  var closeFilter = function () {
+    if (!filterContainer.classList.contains('filter__container--closed')) {
+      filterContainer.classList.add('filter__container--closed');
+      unblockPage();
+      filterFocusTrap.deactivate();
+
+      filterCloseButton.removeEventListener('click', onFilterCloseButtonClick);
+      filterContainer.removeEventListener('click', onFilterContainerOverflowClick);
+      document.removeEventListener('keydown', onFilterKeydown);
+    }
+  };
+
+  var onFilterCloseButtonClick = function (evt) {
+    evt.preventDefault();
+    closeFilter();
+  };
+
+  /* Функция закрытия фильтра при клике на оверфлоу */
+
+  var onFilterContainerOverflowClick = function (evt) {
+    if (evt.target === filterContainer) {
+      closeFilter();
+    }
+  };
+
+  /* Функция закрытия фильтра при нажатии на Escape */
+
+  var onFilterKeydown = function (evt) {
+    if (evt.keyCode === 27) {
+      closeFilter();
+    }
+  };
+
+  /* Функция открытия списка чекбоксов фильтра */
+
+  var onFilterButtonClick = function (evt) {
+    var element;
+
+    if (filterButtonsArray.indexOf(evt.target) >= 0) {
+      element = evt.target;
+    } else {
+      if (filterButtonsArray.indexOf(evt.target.parentElement) >= 0) {
+        element = evt.target.parentElement;
+      } else {
+        element = evt.target.parentElement.parentElement;
+      }
+    }
+
+    var elementParent = element.parentElement;
+
+    if (elementParent.classList.contains('filter__feature--active')) {
+      elementParent.classList.remove('filter__feature--active');
+    } else {
+      elementParent.classList.add('filter__feature--active');
+    }
+  };
+
+  // Функция добавления обработчика собития на кнопку аккордеона
+
+  var addFilterButtonClickHandler = function (button) {
+    button.addEventListener('click', onFilterButtonClick);
+  };
+
+  // Добавляем обработчики на все кнопки аккордеона
+
+  if (filter) {
+    if (filterButtons.length !== 0) {
+      for (var k = 0; k < filterButtons.length; k++) {
+        addFilterButtonClickHandler(filterButtons[k]);
       }
     }
   }
